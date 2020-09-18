a354e1bc6d1d6698d7d3193d643cc31f29502f61
Knut Hellan <knut@lysesvart.no>
2020-09-18 16:09:23 +0200

    Remove all matplotlib
    Remove the possibility of running show_examples.

--- hymenoptera/train.py
+++ hymenoptera/train.py
@@ -86,59 +86,12 @@ def train_model(
     return model, best_acc
 
 
-def imshow(inp, title=None):
-    """Imshow for Tensor."""
-    inp = inp.numpy().transpose((1, 2, 0))
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    inp = std * inp + mean
-    inp = np.clip(inp, 0, 1)
-    plt.imshow(inp)
-    if title is not None:
-        plt.title(title)
-    plt.pause(0.001)  # pause a bit so that plots are updated
+def replace_final_layer(model, num_classes):
+    num_features = model.fc.in_features
+    model.fc = nn.Linear(num_features, num_classes)
+    return model
 
 
-def visualize_model(model, dataloaders, class_names, num_images=6):
-    import matplotlib.pyplot as plt
-
-    was_training = model.training
-    model.eval()
-    images_so_far = 0
-    fig = plt.figure()
-
-    with torch.no_grad():
-        for i, (inputs, labels) in enumerate(dataloaders["val"]):
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-
-            outputs = model(inputs)
-            _, preds = torch.max(outputs, 1)
-
-            for j in range(inputs.size()[0]):
-                images_so_far += 1
-                ax = plt.subplot(num_images // 2, 2, images_so_far)
-                ax.axis("off")
-                ax.set_title("predicted: {}".format(class_names[preds[j]]))
-                imshow(inputs.cpu().data[j])
-
-                if images_so_far == num_images:
-                    model.train(mode=was_training)
-                    return
-        model.train(mode=was_training)
-
-
-def show_examples(dataloaders, class_names):
-    import matplotlib.pyplot as plt
-
-    # Get a batch of training data
-    inputs, classes = next(iter(dataloaders["train"]))
-
-    # Make a grid from batch
-    out = torchvision.utils.make_grid(inputs)
-
-    imshow(out, title=[class_names[x] for x in classes])
-    input("Press Enter to continue...")
 
 
 def fine_tune_train(
@@ -325,8 +278,6 @@ if __name__ == "__main__":
         if is_azure(args):
             run.log("Best Accuracy", np.float(best_accuracy))
         save_model(model, args.model_dir)
-    elif args.action == "show_examples":
-        show_examples(dataloaders, class_names)
     else:
         print(f"Unknown argument {args.action}")
         exit(-1)
